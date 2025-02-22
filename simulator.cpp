3bad782fe401141a8fa4aef51601429885243de6
Ampherost <Ampherost235@gmail.com>
2025-02-21 16:31:20 -0800

    getters and setters

--- simulator.cpp
+++ simulator.cpp
@@ -1,1 +1,56 @@
 #include "simulator.h"
+
+// Getters
+std::string Simulator::getMode() const {
+    return mode;
+}
+
+float Simulator::getPayoutRatio() const {
+    return payoutRatio;
+}
+
+int Simulator::getNumPicks() const {
+    return numPicks;
+}
+
+float Simulator::getLineupOdds() const {
+    return lineupOdds;
+}
+
+
+
+// Setters
+void Simulator::setMode(std::string s) {
+    mode = s;
+}
+
+void Simulator::setPayoutRatio(float f) {
+    payoutRatio = f;
+}
+
+void Simulator::setNumPicks(int n) {
+    numPicks = n;
+}
+
+void Simulator::setLineupOdds(float f) {
+    lineupOdds = f;
+}
+
+
+
+// Display Values
+void Simulator::printMode() const {
+    std::cout << mode << std::endl;
+}
+
+void Simulator::printPayoutRatio() const {
+    std::cout << payoutRatio << std::endl;
+}
+
+void Simulator::printNumPicks() const {
+    std::cout << numPicks << std::endl;
+}
+
+void Simulator::printLineupOdds() const {
+    std::cout << lineupOdds << std::endl;
+}
--- simulator.h
+++ simulator.h
@@ -1,11 +1,43 @@
 #ifndef SIMULATOR_H
 #define SIMULATOR_H
+#include <iostream>
+#include <string>
 
 class Simulator {
 private:
+std::string mode;       // include power and flex plays
+float payoutRatio;      // how much you will get paid 
+int numPicks;           // how many picks you have in your lineup
+float lineupOdds;       // The probability of a single hit
 
 
 public:
+// Getters
+std::string getMode() const;
+float getPayoutRatio() const;
+int getNumPicks() const;
+float getLineupOdds() const;
+
+
+// Setters
+void setMode(std::string s);
+void setPayoutRatio(float f);
+void setNumPicks(int n);
+void setLineupOdds(float f);
+
+
+// Display values
+void printMode() const;
+void printPayoutRatio() const;
+void printNumPicks() const;
+void printLineupOdds() const;
+
+// Calculate numbers
+float calculatePower() const;
+float calculateFlex() const;
+
+
+
 
 };
 
